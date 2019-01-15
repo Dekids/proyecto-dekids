fb9a4a13e11ff033d28948886cc08e83e2eb8b4f
Carlos Hernández <CarlosHdz_97@outlook.com>
2019-01-14 22:02:42 -0600

    ultimos cambios

--- seeders/20181129170736-generate_users.js
+++ seeders/20181129170736-generate_users.js
@@ -3,19 +3,13 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [
-      {id: 1,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Abraham Azael Hernandez Hernandez',  dekids_coins:85, username:'AbrahamHdz', profile:'abraham.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 2,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Alexander Armenta Mejia',  dekids_coins:85, username:'AlexArmenta', profile:'alex.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 3,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Carlos Alejandro Padilla del Angel',  dekids_coins:85, username:'CarlosAle', profile:'carlosAle.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 4,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Carlo Raul Sanchez Flores',  dekids_coins:99, username:'CarloRaul', profile:'carloRaul.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 5,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Citlali Arilyn Hernandez Hernandez',  dekids_coins:89, username:'CitlaliHdz', profile:'citlati.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 6,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Dante Negrete Maiz',  dekids_coins:111, username:'DanteN', profile:'dante.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 7,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Damian Negrete Maiz',  dekids_coins:100, username:'DamianN', profile:'damian.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 8,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Diego Fernando Padilla Angel',  dekids_coins:89, username:'DiegoFer', profile:'diegoFer.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 9,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Diego Gonzales Martinez',  dekids_coins:94, username:'DiegoGonz', profile:'diegoGonzales.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 10,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Emilio Ribelles Marin',  dekids_coins:99, username:'EmilioRibelles', profile:'emilio.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 11,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Gael Ribelles Marin',  dekids_coins:99, username:'GaelRibelles', profile:'gael.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 12,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Mikael Bonilla Nuñez',  dekids_coins:89, username:'MikaelBonilla', profile:'karlo.png', createdAt: new Date(), updatedAt: new Date()},
-      {id: 13,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Karlo Vazquez Valentin',  dekids_coins:109, username:'KarlosVaz', profile:'mikael.png', createdAt: new Date(), updatedAt: new Date()}
+      {id: 14,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Anthony A. Da luz Espinoza',  dekids_coins:0, username:'Anthony A.', profile:'anthony.png', createdAt: new Date(), updatedAt: new Date()},
+      {id: 15,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Aldo Mariano Tamayo Guerrero',  dekids_coins:0, username:'Aldo Tamayo', profile:'mariano.png', createdAt: new Date(), updatedAt: new Date()},
+      {id: 16,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Danna Tamara Valente Limón',  dekids_coins:0, username:'Danna Valente', profile:'danna.png', createdAt: new Date(), updatedAt: new Date()},
+      {id: 17,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Ayax Omar Valente Limón',  dekids_coins:0, username:'Ayax Valente', profile:'ayax.png', createdAt: new Date(), updatedAt: new Date()},
+      {id: 18,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Alexia Vázquez Villalpando',  dekids_coins:0, username:'Alexia Vazquez', profile:'alexia.png', createdAt: new Date(), updatedAt: new Date()},
+      {id: 19,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Christopher Adone Cetto Vaca',  dekids_coins:0, username:'Christopher Adone', profile:'christopher.png', createdAt: new Date(), updatedAt: new Date()},
+      {id: 20,password_hash: '$2b$10$8jGhgJcdL7DOPZDFUTVcT.bsUteS/D6aMDJQgbi49iLvY2oN4Rmga', name:'Damian Negrete Maiz',  dekids_coins:0, username:'Sofia', profile:'sofia.png', createdAt: new Date(), updatedAt: new Date()}
     ], {});
   },
 
--- seeders/20181129170809-generate_skills.js
+++ seeders/20181129170809-generate_skills.js
@@ -3,19 +3,18 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('skills', [
-      {id:1, name: 'Pensamiento computacional',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:2, name: 'Herramientas digitales',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:3, name: 'Logica de programacion',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:4, name: 'Diseño web',dekids_coins: 100,createdAt: new Date(),updatedAt: new Date()},
-      {id:5, name: 'Comunicación',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:6, name: 'Trabajo en equipo',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:7, name: 'Pensamiento innovador',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:8, name: 'Pensamiento creativo',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:9, name: 'Pensamiento critico',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:10, name: 'Solución de problemas',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:11, name: 'Negociación',dekids_coins: 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:12, name: 'Finanzas',dekids_coins : 50,createdAt: new Date(),updatedAt: new Date()},
-      {id:13, name: 'Bonus',dekids_coins : 400,createdAt: new Date(),updatedAt: new Date()}
+      {id:14, name: 'Pensamiento computacional',dekids_coins: 2,createdAt: new Date(),updatedAt: new Date()},
+      {id:15, name: 'Herramientas digitales',dekids_coins: 3,createdAt: new Date(),updatedAt: new Date()},
+      {id:16, name: 'Logica de programacion',dekids_coins: 2,createdAt: new Date(),updatedAt: new Date()},
+      {id:17, name: 'Diseño web',dekids_coins: 36,createdAt: new Date(),updatedAt: new Date()},
+      {id:18, name: 'Comunicación',dekids_coins: 1,createdAt: new Date(),updatedAt: new Date()},
+      {id:19, name: 'Trabajo en equipo',dekids_coins: 11,createdAt: new Date(),updatedAt: new Date()},
+      {id:20, name: 'Pensamiento innovador',dekids_coins: 10, createdAt: new Date(),updatedAt: new Date()},
+      {id:21, name: 'Pensamiento creativo',dekids_coins: 21, createdAt: new Date(),updatedAt: new Date()},
+      {id:22, name: 'Pensamiento critico',dekids_coins: 20, createdAt: new Date(),updatedAt: new Date()},
+      {id:23, name: 'Solución de problemas',dekids_coins: 1,createdAt: new Date(),updatedAt: new Date()},
+      {id:24, name: 'Negociación',dekids_coins: .1,createdAt: new Date(),updatedAt: new Date()},
+      {id:25, name: 'Finanzas',dekids_coins : .1 ,createdAt: new Date(),updatedAt: new Date()}
     ], {});
   },
 
